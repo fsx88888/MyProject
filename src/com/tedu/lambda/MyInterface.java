59cd433ffb22a7d1d20ee757b1e2da7cac77a2fa
fsx88888 <1067836055@qq.com>
2020-07-27 16:49:37 +0800

    测试上传

--- src/com/tedu/lambda/MyInterface.java
+++ src/com/tedu/lambda/MyInterface.java
@@ -4,7 +4,7 @@ public interface MyInterface {
     public abstract void a(); //本接口新定义的的抽象
     public abstract String toString();//和Object 中重名
     public abstract  boolean equals(Object obj);//和Object 中重名
-    //toString()和equals（）看似是抽象方法，实际是有方法体的具体方法
+    //toString()和equals（）看似是抽象方法，实际是有方法体的具体方法，所以本接口只有一个抽象
 }
 
 class  MyInterfaceImpl implements  MyInterface{
