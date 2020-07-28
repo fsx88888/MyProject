76bbedd7326ff88239c41e8315874371ef73c9b1
fsx88888 <1067836055@qq.com>
2020-07-28 16:02:07 +0800

    Collection 学习

---
+++ src/com/tedu/collection/List01.java
@@ -0,0 +1,16 @@
+package com.tedu.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * List是有序的集合
+ * */
+public class List01 {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println(list);
+    }
+}
